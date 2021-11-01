d807067b133f44d81ed2f2ec04823778c3db48bf
sahiljsy <sahiljariwala6@gmail.com>
2021-11-01 10:14:14 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineExamPrepration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
