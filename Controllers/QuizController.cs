ab707f40a8d83eb4274a68afd674b11836ff79df
Aakarsh Inamdar <inamdaraakarsh@gmail.com>
2021-11-02 13:53:17 +0530

    Management of Quiz

---
+++ Controllers/QuizController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineExamPrepration.Controllers
+{
+    public class QuizController: Controller
+    {
+        public IActionResult AddQuiz()
+        {
+            return View();
+        }
+
+        public IActionResult ViewQuiz()
+        {
+            return View();
+        }
+        public IActionResult UpdateQuiz()
+        {
+            return View();
+        }
+    }
+}
